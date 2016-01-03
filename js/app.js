a421ff1816a91c2dd39c21936ffd7255fd246a3f
Jtbradley <jtbradley1@gmail.com>
2016-01-03 06:13:03 -0800

    Finished primary scripts
    Final App 1.0

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(400);
+	$(".what").click(function(){
+		$(".overlay").fadeIn(400);
 
-  	});
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(400);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(400);
+	});
 
   	var randInt; // make accessible in global context
   	function getRandom(min, max) { // (min = included, max =excluded)
@@ -23,21 +23,62 @@ $(document).ready(function(){
 
   	var userNum; // make accessible in global context
   	$( "#mainForm" ).submit(function() {
+  		
   		event.preventDefault();
   		userNum = document.getElementById('userGuess').value; 
-  		console.log(userNum);		
+  		console.log(userNum);	
 
-  		if (userNum > randInt) {
-		alert("too high");
-	};
-	});
+  		var numDiff = userNum - randInt; // the difference	
 
-	
+  		if (userNum == randInt) {
+  			alert("CONGRATULATIONS!! YOU WIN! YOU ARE ULTIMATE HOTTIE!!!! ;)");
+  		}
+  		else if (Math.abs(numDiff)<= 3) {
+  			alert("You are practically on FIRE!!!!!");
+  		}
+  		else if (Math.abs(numDiff)<= 5) {
+  			alert("You are SCORCHING Hot!! >:D" );
+  		}	
+  		else if (Math.abs(numDiff)<= 10) {
+  			alert("Almost there!! You are very hot!");
+  		}
+  		else if (Math.abs(numDiff)<= 20) {
+  			alert("You are quite warm.");
+  		}
+  		else if (Math.abs(numDiff)<= 30) {
+  			alert(" Let's just say you're not cold...");
+  		}
+  		else if (Math.abs(numDiff)<= 40) {
+  			alert("Meh, room temp. You can do better.");
+  		}
+  		else if (Math.abs(numDiff)<= 50) {
+  			alert("You need a jacket?");
+  		}
+  		else if (Math.abs(numDiff)<= 60) {
+  			alert("You are very, very cold. I am sorry.");
+  		}
+  		else {
+  			alert("LOL You are FREEZING cold... Not even close.");
+
+  		}
+
+  		counter ++; // increment the count on submit
+  		$("#count").html(counter);
+
+  	});
+
+  	var counter = 0;
+
+  	$(".new").click(function() {
+  		location.reload();
+  	});
+  	
+
+  	
 
 
-	
 
 
-});
+  });
 
 
