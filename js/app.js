8eb10cd5cdf172fa275872b1475eefb224f92e3e
Jtbradley <jtbradley1@gmail.com>
2016-01-03 04:43:48 -0800

    initial commit

--- js/app.js
+++ js/app.js
@@ -3,14 +3,40 @@ $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+    	$(".overlay").fadeIn(400);
 
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
+  		$(".overlay").fadeOut(400);
   	});
+
+  	var randInt; // make accessible in global context
+  	function getRandom(min, max) { // (min = included, max =excluded)
+  		randInt= Math.abs(Math.floor(Math.random() * (min - max)) + min);
+  		console.log(randInt);
+  		return(randInt);
+  	};
+
+  	getRandom(1, 101);
+
+  	var userNum; // make accessible in global context
+  	$( "#mainForm" ).submit(function() {
+  		event.preventDefault();
+  		userNum = document.getElementById('userGuess').value; 
+  		console.log(userNum);		
+
+  		if (userNum > randInt) {
+		alert("too high");
+	};
+	});
+
+	
+
+
+	
+
 
 });
 
